22db2375009d2e000d6536af847e9d38556a2e7c
Qin XIA <qxia@e1r5p10.clusters.42paris.fr>
2022-08-29 15:06:38 +0200

    check map

--- src/parser/check_map_util.c
+++ src/parser/check_map_util.c
@@ -6,7 +6,59 @@
 /*   By: qxia <qxia@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/12 13:26:27 by qxia              #+#    #+#             */
-/*   Updated: 2022/08/12 13:26:28 by qxia             ###   ########.fr       */
+/*   Updated: 2022/08/29 15:05:17 by qxia             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "cub3d.h"
+
+int check_filename(char *path)
+{
+    char    *str;
+
+    str = ft_strrchr(path, '.');
+    if (!str)
+        return (1);
+    str++;
+    if (ft_strcmp(str, "xpm"))
+        return (1);
+    return (0);
+}
+
+int ft_check_fd(t_info *info)
+{
+    int fd;
+
+    if (!info->no || !info->so || !info->we || !info->ea)
+		return (ERR_ACCESS);
+	if (check_filename(info->no) || check_filename(info->so) || \
+		check_filename(info->we) || check_filename(info->ea))
+		return (ERR_ACCESS);
+	fd = open(info->no, O_RDONLY);
+	if (fd <= 0)
+		return (ERR_ACCESS); // do not define yet
+	close(fd);
+	fd = open(info->so, O_RDONLY);
+	if (fd <= 0)
+		return (ERR_ACCESS);
+	close(fd);
+	fd = open(info->we, O_RDONLY);
+	if (fd <= 0)
+		return (ERR_ACCESS);
+	close(fd);
+	fd = open(info->ea, O_RDONLY);
+	if (fd <= 0)
+		return (ERR_ACCESS);
+	close(fd);
+	return (0);
+}
+
+int ft_check_info(t_info *info)
+{
+    if (!info->no || !info->so || !info->we || !info->ea || \
+            !info->f || !info->c)
+            return (ERR_INFO);
+    if (ft_check_fd(info) != 0)
+            return (ERR_ACCESS);
+    return (0);
+}
